b50fcc573e952f8d4bec1e35550c454ccbbabdb4
Stephan Casas <stephancasas@icloud.com>
2021-02-02 00:27:04 -0600

    Created CLI project init option

--- utils/options/options.js
+++ utils/options/options.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const commandLineArgs=require("command-line-args"),args=[{name:"config",alias:"c",type:String,defaultValue:"snowblade.config.js",defaultOption:!0}];exports.default=commandLineArgs(args);
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const commandLineArgs=require("command-line-args"),args=[{name:"config",alias:"c",type:String,defaultValue:"snowblade.config.js",defaultOption:!0},{name:"init",alias:"i",type:Boolean,defaultValue:!1}];exports.default=commandLineArgs(args);
